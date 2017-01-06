5ff2eb34fcc63806d80810b836327654fb5db167
Andrew Chumich <andrewchumich@gmail.com>
2017-01-06 13:53:07 -0700

    remove log

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ var app = express()
 export const STATE_CHANGE = 'STATE_CHANGE';
 // start pinball app
 var sse = initializeSSE()
-const ScoreEmitter = new EventEmitter() 
+const ScoreEmitter = new EventEmitter()
 
 // this is a Server-Side Events endpoint
 app.get('/scores', (req, res) => {
@@ -27,7 +27,6 @@ app.get('/scores', (req, res) => {
 
     req.on('close', () => {
         ScoreEmitter.removeListener('score', event_callback)
-        console.log(ScoreEmitter.listeners('score'))
     })
 })
 
@@ -44,36 +43,3 @@ Pinball.setUser('ALC');
 Pinball.start()
 
 app.listen(3000)
-
-/*app.get('/', (req, res) => {
-    res.send('hello world')
-})*/
-
-/*var io = require('socket.io')(server)
-io.on('connection', (socket) => {
-    console.log('HELLO WORLD')
-    // on connection we should start sending score data from PinballScore
-
-    const config: PinballConfig = {
-        onStateChange: (state: pinballState) => {
-            console.log(state)
-            io.emit(STATE_CHANGE, JSON.stringify(state));
-        }
-    };
-
-    Pinball.setConfig(config);
-
-    Pinball.setUser('ALC');
-
-});
-
-Pinball.start()
-
-
-
-function originIsAllowed(origin) {
-    // put logic here to detect whether the specified origin is allowed.
-    return true;
-}
-*/
-//server.listen(3000);
